77fde5fecf2582fefbd409b232819e71babc4ce2
kelly-Moon <kellymoon220@gmail.com>
2025-11-18 09:00:52 +0900

    섹션 컴퍼넌트 구조화하기

--- src/App.jsx
+++ src/App.jsx
@@ -12,24 +12,31 @@ import Channel from "./pages/Channel";
 import Video from "./pages/Video";
 import Search from "./pages/Search";
 import Not from "./pages/Not";
+import Header from "./components/section/Header";
+import Main from "./components/section/Main";
+import Footer from "./components/section/Footer";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/today" element={<Today />} />
-        <Route path="/travel" element={<Travel />} />
-        <Route path="/movie" element={<Movie />} />
-        <Route path="/music" element={<Music />} />
-        <Route path="/fashion" element={<Fashion />} />
-        <Route path="/recipe" element={<Recipe />} />
-        <Route path="/study" element={<Study />} />
-        <Route path="/channel/:channelID" element={<Channel />} />
-        <Route path="/video/:videoID" element={<Video />} />
-        <Route path="/search/:searchID" element={<Search />} />
-        <Route path="/*" element={<Not />} />
-      </Routes>
+      <Header />
+      <Main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/today" element={<Today />} />
+          <Route path="/travel" element={<Travel />} />
+          <Route path="/movie" element={<Movie />} />
+          <Route path="/music" element={<Music />} />
+          <Route path="/fashion" element={<Fashion />} />
+          <Route path="/recipe" element={<Recipe />} />
+          <Route path="/study" element={<Study />} />
+          <Route path="/channel/:channelID" element={<Channel />} />
+          <Route path="/video/:videoID" element={<Video />} />
+          <Route path="/search/:searchID" element={<Search />} />
+          <Route path="/*" element={<Not />} />
+        </Routes>
+      </Main>
+      <Footer />
     </BrowserRouter>
   );
 };
