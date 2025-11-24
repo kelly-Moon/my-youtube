2ebcd8d1df0c4ed94d47b2678949381b206a653a
kelly-Moon <kellymoon220@gmail.com>
2025-11-24 23:25:51 +0900

    추천 크리에이터 작업

--- src/components/contents/Creator.jsx
+++ src/components/contents/Creator.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { creatorText } from "../../data/creator";
+import { Link } from "react-router-dom";
 
 const Creator = () => {
-  return <div>Creator</div>;
+  return (
+    <section id="creator">
+      <h2>😎 추천 크리에이터를 소개합니다.</h2>
+      <div className="creator__inner overflow">
+        {creatorText.map((creator, key) => (
+          <div className="creator" key={key}>
+            <div className="creator_img play__icon ">
+              <Link to={`/channel/${creator.channelId}`}>
+                <img src={creator.img} alt={creator.name} />
+              </Link>
+            </div>
+            <div className="creator_info">
+              <Link to={`/channel/${creator.channelId}`}>{creator.name}</Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default Creator;
--- src/pages/Creators.jsx
+++ src/pages/Creators.jsx
@@ -1,10 +1,28 @@
 import React from "react";
 import Main from "../components/section/Main";
+import { creatorText } from "../data/creator";
+import { Link } from "react-router-dom";
 
 const Creators = () => {
   return (
     <Main title="추천 크리에티터" description="오늘의 추천 크리에티터입니다.">
-      Creators
+      <section id="creatorPage">
+        <h2>😎 추천 크리에이터를 소개합니다.</h2>
+        <div className="creator__inner">
+          {creatorText.map((creator, key) => (
+            <div className="creator" key={key}>
+              <div className="creator_img play__icon ">
+                <Link to={`/channel/${creator.channelId}`}>
+                  <img src={creator.img} alt={creator.name} />
+                </Link>
+              </div>
+              <div className="creator_info">
+                <Link to={`/channel/${creator.channelId}`}>{creator.name}</Link>
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
     </Main>
   );
 };
